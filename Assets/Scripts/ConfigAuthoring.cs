b4a82349a990898ddbe940e805867ca22886ce52
Efaideleon <efaideleon@gmail.com>
2025-02-10 15:10:19 -0800

    message

---
+++ Assets/Scripts/ConfigAuthoring.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Unity.Entities;
+
+public class ConfigAuthoring : MonoBehaviour
+{
+    [Header("Settings")]
+    public string Name;
+    public int Id;
+    public string Color;
+
+    [Header("Prefabs")]
+    public GameObject CubePrefab;
+
+    class Baker : Baker<ConfigAuthoring>  
+    {
+        public override void Bake(ConfigAuthoring authoring)
+        {
+            var entity = GetEntity(authoring, TransformUsageFlags.None); 
+            AddComponent(entity, new Config
+            {
+                Name = authoring.Name,
+                Id = authoring.Id,
+                Color = authoring.Color,
+                cubePrefab = GetEntity(authoring.CubePrefab, TransformUsageFlags.Dynamic),
+            });
+
+        }
+    }
+
+    public struct Config : IComponentData
+    {
+        public string Name;
+        public int Id;
+        public string Color;
+        public Entity cubePrefab;
+    }
+}
